8b162f00114c95621ca6b5569f8901183c28d1b7
andrey <poizen211@gmail.com>
2024-02-09 15:08:36 +0300

    chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,28 +7,59 @@ import {UUID} from "node:crypto";
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.string(),
 });
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
-export async function createInvoice(formData: FormData) {
 
-    const { customerId, amount, status } =CreateInvoice.parse( {
+
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+
+
+export async function createInvoice(prevState:State,formData: FormData) {
+
+    const validatedFields =CreateInvoice.safeParse( {
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
 });
+    if(!validatedFields.success)
+    {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
-    await sql`
+    try{await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  `;}
+    catch (error)
+    {
+        return{message:"DB Error: cannot create invoice"}
+    }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
@@ -42,11 +73,16 @@ export async function updateInvoice (formData:FormData,id:string )
     });
     const amountInCents = amount * 100;
 
-    await sql`
+    try{
+        await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
-  `;
+  `;}
+    catch (error)
+    {
+        return{message:"DB Error: cannot update invoice"}
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
@@ -54,7 +90,13 @@ export async function updateInvoice (formData:FormData,id:string )
 
 export async function  deleteInvoice(id:string)
 {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    try {
+        await sql`DELETE FROM invoices WHERE id = ${id}`;
+    }
+    catch (error)
+    {
+        return {message:"DB Error:cannot delete invoice"}
+    }
     revalidatePath('/dashboard/invoices');
 }
 

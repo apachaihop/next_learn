3776aa544fa62446a034d51a2e69eb5e910597de
andrey <poizen211@gmail.com>
2024-02-20 18:04:24 +0300

    chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,9 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import {UUID} from "node:crypto";
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 
 const FormSchema = z.object({
     id: z.string(),
@@ -98,5 +101,23 @@ export async function  deleteInvoice(id:string)
         return {message:"DB Error:cannot delete invoice"}
     }
     revalidatePath('/dashboard/invoices');
+}
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+) {
+    try {
+        await signIn('credentials', formData);
+    } catch (error) {
+        if (error instanceof AuthError) {
+            switch (error.type) {
+                case 'CredentialsSignin':
+                    return 'Invalid credentials.';
+                default:
+                    return 'Something went wrong.';
+            }
+        }
+        throw error;
+    }
 }
 
